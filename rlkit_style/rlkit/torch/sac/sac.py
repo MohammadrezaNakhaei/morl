140cbf2eb6352019cf93299599a78b115e340c39
Mohammadreza <mohammadreza.mrn2010@gmail.com>
2024-06-28 11:38:14 +0300

    add pred objective

--- rlkit_style/rlkit/torch/sac/sac.py
+++ rlkit_style/rlkit/torch/sac/sac.py
@@ -449,3 +449,284 @@ class CSROSoftActorCritic(OfflineMetaRLAlgorithm):
             c=self.c.state_dict(),
             )
         return snapshot
+    
+    
+    
+class CSROPrediction(CSROSoftActorCritic):
+    def __init__(
+            self,
+            env,
+            train_tasks,
+            eval_tasks,
+            extreme_tasks,
+            latent_dim,
+            nets,
+            decoder,
+            optimizer_class=optim.Adam,
+            **kwargs,
+        ):
+        super().__init__(
+                env,
+                train_tasks,
+                eval_tasks,
+                extreme_tasks,
+                latent_dim,
+                nets,
+                optimizer_class=optim.Adam,
+                **kwargs,
+            )   
+        
+        self.decoder = decoder
+        self.prediction_loss_weight         = kwargs['prediction_loss_weight']
+        self.use_prediction                 = kwargs['use_prediction']  
+        self.pred_criterion                 = nn.MSELoss()
+        
+        self.context_optimizer = optimizer_class(
+            list(self.agent.context_encoder.parameters()) + list(self.decoder.parameters()), 
+            lr=self.context_lr,
+            )
+        # state/reward normalizers for better hyperparameters
+        normal_idx = self.n_tasks//2
+        size = self.replay_buffer.task_buffers[normal_idx].size()
+        obss = self.replay_buffer.task_buffers[normal_idx]._observations[:size]
+        rewards = self.replay_buffer.task_buffers[normal_idx]._rewards[:size]
+        self.mu_state = ptu.FloatTensor(np.mean(obss, axis=0))
+        self.std_state = ptu.FloatTensor(np.std(obss, axis=0) + 1e-6)
+        self.mu_reward = ptu.FloatTensor(np.mean(rewards, 0))
+        self.std_reward = ptu.FloatTensor(np.std(rewards, 0) + 1e-6)
+    
+    @property
+    def networks(self):
+        return self.agent.networks + [self.qf1, self.qf2, self.vf, self.target_vf, self.c, self.club_model, self.decoder]
+
+
+    def _take_step(self, indices, context):
+        obs_dim = int(np.prod(self.env.observation_space.shape))
+        action_dim = int(np.prod(self.env.action_space.shape))
+        reward_in_context = context[:, :, obs_dim + action_dim].cpu().numpy()
+        self.loss["non_sparse_ratio"] = len(reward_in_context[np.nonzero(reward_in_context)]) / np.size(reward_in_context)
+
+        num_tasks = len(indices)
+        # data is (task, batch, feat)
+        obs, actions, rewards, next_obs, terms = self.sample_sac(indices)
+
+        policy_outputs, task_z, task_z_vars= self.agent(obs, context, task_indices=indices)
+
+        # flattens out the task dimension
+        t, b, _ = obs.size()
+        obs = obs.view(t * b, -1)
+        actions = actions.view(t * b, -1)
+        next_obs = next_obs.view(t * b, -1)
+        rewards_flat = rewards.view(self.batch_size * num_tasks, -1)
+        new_actions, policy_mean, policy_log_std, log_pi = policy_outputs[:4]
+
+        # Q and V networks
+        # encoder will only get gradients from Q nets
+        if self.allow_backward_z:
+            q1_pred = self.qf1(t, b, obs, actions, task_z)
+            q2_pred = self.qf2(t, b, obs, actions, task_z)
+            v_pred = self.vf(t, b, obs, task_z.detach())
+        else:
+            q1_pred = self.qf1(t, b, obs, actions, task_z.detach())
+            q2_pred = self.qf2(t, b, obs, actions, task_z.detach())
+            v_pred = self.vf(t, b, obs, task_z.detach())
+        # get targets for use in V and Q updates
+        # BRAC:
+        # div_estimate = self._divergence.dual_estimate(
+        #     s2, a2_p, a2_b, self._c_fn)
+        
+        c_loss = self._divergence.dual_critic_loss(obs, new_actions.detach(), actions, task_z.detach())
+        self.c_optimizer.zero_grad()
+        c_loss.backward(retain_graph=True)
+        self.c_optimizer.step()
+        for _ in range(self._c_iter - 1):
+            self._optimize_c(indices=indices, context=context)
+        self.loss["c_loss"] = c_loss.item()
+
+        div_estimate = self._divergence.dual_estimate(
+            obs, new_actions, actions, task_z.detach())
+        self.loss["div_estimate"] = torch.mean(div_estimate).item()
+
+        with torch.no_grad():
+            if self.use_brac and self.use_value_penalty:
+                target_v_values = self.target_vf(t, b, next_obs, task_z) - self.get_alpha * div_estimate
+            else:
+                target_v_values = self.target_vf(t, b, next_obs, task_z)
+        self.loss["target_v_values"] = torch.mean(target_v_values).item()
+
+        if self.use_club:
+            self.club_model_optimizer.zero_grad()
+            z_target = self.agent.encode_no_mean(context).detach()
+            z_param = self.club_model(context[...,:self.club_model.input_size])
+            z_mean = z_param[..., :self.latent_dim]
+            z_var = F.softplus(z_param[..., self.latent_dim:])
+            club_model_loss = self.club_model_loss_weight * ((z_target - z_mean)**2/(2*z_var) + torch.log(torch.sqrt(z_var))).mean()
+            club_model_loss.backward()
+            self.loss["club_model_loss"] = club_model_loss.item()
+            self.club_model_optimizer.step()
+
+        # KL constraint on z if probabilistic
+        self.context_optimizer.zero_grad()
+
+        if self.use_club:
+            z_target = self.agent.encode_no_mean(context)
+            z_param = self.club_model(context[...,:self.club_model.input_size]).detach()
+            z_mean = z_param[..., :self.latent_dim]
+            z_var = F.softplus(z_param[..., self.latent_dim:])
+            z_t, z_b, _ = z_mean.size()
+            position = - ((z_target-z_mean)**2/z_var).mean()
+            z_mean_expand = z_mean[:, :, None, :].expand(-1, -1, z_b, -1).reshape(z_t, z_b**2, -1)
+            z_var_expand = z_var[:, :, None, :].expand(-1, -1, z_b, -1).reshape(z_t, z_b**2, -1)
+            z_target_repeat = z_target.repeat(1, z_b, 1)
+            negative = - ((z_target_repeat-z_mean_expand)**2/z_var_expand).mean()
+            club_loss = self.club_loss_weight * (position - negative)
+            club_loss.backward(retain_graph=True)
+            self.loss["club_loss"] = club_loss.item()
+
+        if self.use_FOCAL_cl:
+            z_loss = self.z_loss_weight * self.FOCAL_z_loss(indices=indices, task_z=task_z, task_z_vars=task_z_vars, b=b)
+            z_loss.backward(retain_graph=True)
+            self.loss["z_loss"] = z_loss.item()
+        
+        if self.use_prediction:
+            # extended_state = torch.cat([obs, actions, task_z], dim=-1)
+            pred = self.decoder(t, b, obs, actions, task_z)
+            target = torch.cat([(next_obs-obs)/self.std_state, (rewards_flat-self.mu_reward)/self.std_reward], dim=-1)
+            prediction_loss = self.prediction_loss_weight * self.pred_criterion(pred, target)
+            prediction_loss.backward(retain_graph=True)
+            self.loss['prediction'] = prediction_loss.item()
+
+        # qf and encoder update (note encoder does not get grads from policy or vf)
+        self.qf1_optimizer.zero_grad()
+        self.qf2_optimizer.zero_grad()
+        # scale rewards for Bellman update
+        rewards_flat = rewards_flat * self.reward_scale
+        terms_flat = terms.view(self.batch_size * num_tasks, -1)
+        q_target = rewards_flat + (1. - terms_flat) * self.discount * target_v_values
+        qf_loss = torch.mean((q1_pred - q_target) ** 2) + torch.mean((q2_pred - q_target) ** 2)
+        qf_loss.backward(retain_graph=True)
+        self.loss["qf_loss"] = qf_loss.item()
+        self.loss["q_target"] = torch.mean(q_target).item()
+        self.loss["q1_pred"] = torch.mean(q1_pred).item()
+        self.loss["q2_pred"] = torch.mean(q2_pred).item()
+
+        self.qf1_optimizer.step()
+        self.qf2_optimizer.step()
+        self.context_optimizer.step()
+
+        # compute min Q on the new actions
+        min_q_new_actions = torch.min(self.qf1(t, b, obs, new_actions, task_z.detach()),
+                                        self.qf2(t, b, obs, new_actions, task_z.detach()))
+
+        # vf update
+        if self.max_entropy:
+            v_target = min_q_new_actions - log_pi
+        else:
+            v_target = min_q_new_actions
+        vf_loss = self.vf_criterion(v_pred, v_target.detach())
+        self.vf_optimizer.zero_grad()
+        vf_loss.backward(retain_graph=True)
+        self.vf_optimizer.step()
+        self._update_target_network()
+        self.loss["vf_loss"] = vf_loss.item()
+        self.loss["v_target"] = torch.mean(v_target).item()
+        self.loss["v_pred"] = torch.mean(v_pred).item()
+
+        # policy update
+        # n.b. policy update includes dQ/da
+        log_policy_target = min_q_new_actions
+
+        # BRAC:
+        if self.use_brac:
+            if self.max_entropy:
+                policy_loss = (log_pi - log_policy_target + self.get_alpha.detach() * div_estimate).mean()
+            else:
+                policy_loss = (-log_policy_target + self.get_alpha.detach() * div_estimate).mean()
+        else:
+            if self.max_entropy:
+                policy_loss = (log_pi - log_policy_target).mean()
+            else:
+                policy_loss = - log_policy_target.mean()
+
+        mean_reg_loss = self.policy_mean_reg_weight * (policy_mean**2).mean()
+        std_reg_loss = self.policy_std_reg_weight * (policy_log_std**2).mean()
+        pre_tanh_value = policy_outputs[-1]
+        pre_activation_reg_loss = self.policy_pre_activation_weight * (
+            (pre_tanh_value**2).sum(dim=-1).mean()
+        )
+        policy_reg_loss = mean_reg_loss + std_reg_loss + pre_activation_reg_loss
+        policy_loss = policy_loss + policy_reg_loss
+
+        self.policy_optimizer.zero_grad()
+        policy_loss.backward(retain_graph=True)
+        self.policy_optimizer.step()
+
+        self.loss["policy_loss"] = policy_loss.item()
+        # optimize for c network (which computes dual-form divergences)
+        # BRAC for training alpha:
+        a_loss = -torch.mean(self._alpha_var * (div_estimate - self._target_divergence).detach())
+        a_loss.backward()
+        with torch.no_grad():
+            self._alpha_var -= self.alpha_lr * self._alpha_var.grad
+            # Manually zero the gradients after updating weights
+            self._alpha_var.grad.zero_()
+        self.loss["a_loss"] = a_loss.item()
+        
+        for i in range(len(self.agent.z_means[0])):
+            z_mean = ptu.get_numpy(self.agent.z_means[0][i])
+            name = f'train/z_mean_{i}'
+            self.eval_statistics[name] = z_mean
+
+        z_sig = np.mean(ptu.get_numpy(self.agent.z_vars[0]))
+
+        self.eval_statistics['train/Z_variance'] = z_sig
+
+        if self.use_club:
+            self.eval_statistics['train/loss_club_model'] = ptu.get_numpy(club_model_loss)
+            self.eval_statistics['train/loss_club'] = ptu.get_numpy(club_loss)
+        if self.use_FOCAL_cl:
+            self.eval_statistics['train/loss_focal'] = ptu.get_numpy(z_loss)
+        if self.use_prediction:
+            self.eval_statistics['train/loss_prediction'] = ptu.get_numpy(prediction_loss)
+
+        self.eval_statistics['train/loss_qf'] = np.mean(ptu.get_numpy(qf_loss))
+        self.eval_statistics['train/loss_vf'] = np.mean(ptu.get_numpy(vf_loss))
+        self.eval_statistics['train/loss_policy'] = np.mean(ptu.get_numpy(
+            policy_loss
+        ))
+        if self.use_brac:
+            self.eval_statistics['train/loss_dual_brack'] = np.mean(ptu.get_numpy(c_loss))
+        self.eval_statistics['train/avg_q_values'] =  ptu.get_numpy(q1_pred).mean()
+        self.eval_statistics['train/avg_v_values'] =  ptu.get_numpy(v_pred).mean()
+        self.eval_statistics['train/log_policy'] =  ptu.get_numpy(log_pi).mean()
+        self.eval_statistics['train/alpha'] =  ptu.get_numpy((self._alpha_var).reshape(-1)).mean()
+        self.eval_statistics['train/div_estimate'] =  ptu.get_numpy(div_estimate).mean()
+        return ptu.get_numpy(self.agent.z_means), ptu.get_numpy(self.agent.z_vars)
+
+    def get_epoch_snapshot(self, epoch):
+        # NOTE: overriding parent method which also optionally saves the env
+        snapshot = OrderedDict(
+            qf1=self.qf1.state_dict(),
+            qf2=self.qf2.state_dict(),
+            policy=self.agent.policy.state_dict(),
+            vf=self.vf.state_dict(),
+            target_vf=self.target_vf.state_dict(),
+            context_encoder=self.agent.context_encoder.state_dict(),
+            club_model=self.club_model.state_dict(),
+            c=self.c.state_dict(),
+            decoder=self.decoder.state_dict()
+            )
+        return snapshot
+    
+    def to(self, device=None):
+        if device == None:
+            device = ptu.device
+        for net in self.networks:
+            net.to(device)
+        if self.train_alpha:
+            self._alpha_var = torch.tensor(self.alpha_init, device=ptu.device, requires_grad=True)
+        self._divergence = divergences.get_divergence(name=self._divergence_name, c=self.c, device=ptu.device)
+        self.mu_state = self.mu_state.to(device)
+        self.std_state = self.std_state.to(device)
+        self.mu_reward = self.mu_reward.to(device)
+        self.std_reward = self.std_reward.to(device)
